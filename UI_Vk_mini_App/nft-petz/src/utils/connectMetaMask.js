197469152c19471d66deab8da04b88b310c9763b
WoolenLamb2171 <romanmurtukov@gmail.com>
2023-07-26 23:19:02 +0300

    added network change

--- UI_Vk_mini_App/nft-petz/src/utils/connectMetaMask.js
+++ UI_Vk_mini_App/nft-petz/src/utils/connectMetaMask.js
@@ -2,6 +2,22 @@ const connectMetaMask = async ()=>{
     if(!window?.ethereum){
         throw new Error("MetaMask is not installed");
     }
+
+    await window.ethereum.request({
+        method: "wallet_addEthereumChain",
+        params: [{
+            chainId: "0x1b198",
+            rpcUrls: ["https://rpc.test.siberium.net"],
+            chainName: "Siberium Testnet",
+            nativeCurrency: {
+                name: "SIBR",
+                symbol: "SIBR",
+                decimals: 18
+            },
+            blockExplorerUrls: ["https://explorer.test.siberium.net/"]
+        }]
+    });
+
     const accounts = await window.ethereum.request({
         method: "eth_requestAccounts",
         params: [], 
