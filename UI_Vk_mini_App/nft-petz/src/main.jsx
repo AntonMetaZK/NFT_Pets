67b4a5848aa6101013dcf6fa7f5c8ed63c97a388
Kireev Kirill <kireev.kd@perfema.com>
2023-07-27 20:04:13 +0300

    fix deploy

--- UI_Vk_mini_App/nft-petz/src/main.jsx
+++ UI_Vk_mini_App/nft-petz/src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { AppContextProvider } from './context/appContext.jsx'
+import bridge from "@vkontakte/vk-bridge";
+
+bridge.send("VKWebAppInit");
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
