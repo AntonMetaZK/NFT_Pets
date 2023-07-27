9770d101887b05466c3f3c19a8965b62a3b22a42
WoolenLamb2171 <romanmurtukov@gmail.com>
2023-07-27 19:42:40 +0300

    fixed vk tunnel port

--- UI_Vk_mini_App/nft-petz/src/App.jsx
+++ UI_Vk_mini_App/nft-petz/src/App.jsx
@@ -15,40 +15,40 @@ function App() {
 
   return (
     <>
-         <button onClick={() =>{handleConnectWalletClick()}} id="connectWallet">Подключить MetaMask</button>
+      <button onClick={() =>{handleConnectWalletClick()}} id="connectWallet">Подключить MetaMask</button>
 
-          <h2>Создание NFT</h2>
-          <input
-            type="number"
-            id="dnaInput"
-            placeholder="Введите ДНК (например, 123456)"
-          />
-          <button id="mintButton">Создать NFT</button>
+      <h2>Создание NFT</h2>
+      <input
+        type="number"
+        id="dnaInput"
+        placeholder="Введите ДНК (например, 123456)"
+      />
+      <button id="mintButton">Создать NFT</button>
 
-          <h2>Скрещивание NFT</h2>
-          <input type="number" id="nft1Id" placeholder="ID первого NFT" />
-          <input type="number" id="nft2Id" placeholder="ID второго NFT" />
-          <button id="breedButton">Скрестить NFT</button>
+      <h2>Скрещивание NFT</h2>
+      <input type="number" id="nft1Id" placeholder="ID первого NFT" />
+      <input type="number" id="nft2Id" placeholder="ID второго NFT" />
+      <button id="breedButton">Скрестить NFT</button>
 
-          <h2>Информация о NFT</h2>
-          <input type="number" id="nftInfoId" placeholder="ID NFT" />
-          <button id="getInfoButton">Получить информацию</button>
-          <div id="nftInfo"></div>
+      <h2>Информация о NFT</h2>
+      <input type="number" id="nftInfoId" placeholder="ID NFT" />
+      <button id="getInfoButton">Получить информацию</button>
+      <div id="nftInfo"></div>
 
-          <script src="ethers.min.js"></script>
-          <script src="app.js"></script>
+      <script src="ethers.min.js"></script>
+      <script src="app.js"></script>
 
-          {currentAccount && (
-            <div id="walletInfo">
-            <strong>Адрес кошелька: {currentAccount.substring(0,6)}...{currentAccount.substring(38)}</strong>
-            <br></br>
-            <strong>Сеть: Siberium Testnet</strong> 
-          </div>
-          )}
+      {currentAccount && (
+        <div id="walletInfo">
+        <strong>Адрес кошелька: {currentAccount.substring(0,6)}...{currentAccount.substring(38)}</strong>
+        <br></br>
+        <strong>Сеть: Siberium Testnet</strong> 
+      </div>
+      )}
 
 
-          <h2>Ваши NFT</h2>
-          <div id="userNFTs">Загрузка...</div>
+      <h2>Ваши NFT</h2>
+      <div id="userNFTs">Загрузка...</div>
     </>
   )
 }
